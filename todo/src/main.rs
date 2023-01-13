2cefe04a3d6ad937f8faa3a24b3f033114844cda
zirski <tobiasjhammond@gmail.com>
2023-01-13 11:31:07 -0800

    user init done; todo: file creation

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, fs};
 use chrono::*;
 struct item {
     name: String,
@@ -7,7 +7,7 @@ struct item {
 }
 
 impl item {
-    fn new(name: String, due_date: &str) -> item { //inputted date should follow YY DD MM format
+    fn new(name: &str, due_date: &str) -> item { //inputted date should follow YY DD MM format
         let now = Utc::now();
         let inputs: Vec<i32> = due_date.split(" ")
                             .map(|x| x.parse::<i32>().unwrap())
@@ -18,22 +18,38 @@ impl item {
         let dt: DateTime<Utc> = DateTime::from_utc(NaiveDateTime::new(d, t), Utc);
 
         let new_item = item {
-            name,
+            name: name.to_string(),
             due_date: dt,
             time_left: now.signed_duration_since(dt),
         };
 
         new_item
-        
     }
 
     fn check(&mut self) {
         let now = Utc::now();
 
         self.time_left = now.signed_duration_since(self.due_date);
-
     }
 }
+
+fn init_user() {
+    let mut entries = fs::read_dir(".").unwrap().collect::<Vec<_>>();
+    let mut filenames: Vec<String> = Vec::new();
+
+    for entry in entries {
+        filenames.push(entry.unwrap().file_name().into_string().unwrap());
+    }
+
+    for v in filenames {
+        if v == String::from("todo_config.txt") {
+            load_user();
+        } else {
+            create_config();
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
